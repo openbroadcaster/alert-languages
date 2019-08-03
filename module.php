d33622ef6249f6fe3815c843ba437701945d09be
Ruby Wolf <ruby.w@pm.me>
2019-08-03 23:21:38 +0000

    Database table fixes.

--- module.php
+++ module.php
@@ -19,24 +19,24 @@ class AlertLanguagesModule extends OBFModule
         'description'=>'alert languages module',
         'name'=>'alert_languages_module'
       ]);
-      
+
       // table to store languages
       $this->db->query('CREATE TABLE IF NOT EXISTS `module_alert_languages` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `code` varchar(50) NOT NULL UNIQUE,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;');
-      
+  			`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  			`name` varchar(255) NOT NULL,
+  			`code` varchar(50) NOT NULL UNIQUE,
+  			PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
+
       // table to store alert media id's
       $this->db->query('CREATE TABLE IF NOT EXISTS `module_alert_languages_alerts` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `language_id` int(10) UNSIGNED NOT NULL,
-  `alert_name` varchar(255) NOT NULL,
-  `media_id` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
-      
+  			`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  			`language_id` int(10) UNSIGNED NOT NULL,
+  			`alert_name` varchar(255) NOT NULL,
+  			`media_id` int(10) UNSIGNED NOT NULL,
+  			PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
+
       return true;
 	}
 
@@ -45,13 +45,13 @@ class AlertLanguagesModule extends OBFModule
       // remove permissions data for this module
       $this->db->where('name','alert_languages_module');
       $permission = $this->db->get_one('users_permissions');
-      
+
       $this->db->where('permission_id',$permission['id']);
       $this->db->delete('users_permissions_to_groups');
-      
+
       $this->db->where('id',$permission['id']);
       $this->db->delete('users_permissions');
-      
+
       return true;
 	}
 }
